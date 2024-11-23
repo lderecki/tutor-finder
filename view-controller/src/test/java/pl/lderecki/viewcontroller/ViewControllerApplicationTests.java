14a30d42ae28f3983de3e08fa7ce78afa8973cf4
lderecki <derecki.lukasz@gmail.com>
2024-11-24 00:24:56 +0100

    Created view-controller with basic configuration of R2DBC datasource.

---
+++ view-controller/src/test/java/pl/lderecki/viewcontroller/ViewControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lderecki.viewcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
